6a6d75d60da0314d1afcd4cde78231ee17c463a2
chenc <git@zzfzzf.com>
2021-07-24 19:57:08 +0800

    feat: webpack插件学习

--- packages/webpack/lib/webpack.js
+++ packages/webpack/lib/webpack.js
@@ -1,12 +1,12 @@
 'use strict';
-const md= require('./test.md').default;
+// const md= require('./test.md').default;
 module.exports = webpack;
 
 function webpack() {
     // TODO
-    console.log(md);
-    const el=document.createElement('div')
-    el.innerHTML=md
-    document.body.appendChild(el)
+    // console.log(md);
+    // const el=document.createElement('div')
+    // el.innerHTML=md
+    // document.body.appendChild(el)
 }
 webpack()
--- packages/webpack/webpack.config.js
+++ packages/webpack/webpack.config.js
@@ -1,26 +1,33 @@
 const path = require("path");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const DingTalkMsgPlugin = require('@zzf-webpack/plugin')
 
 module.exports = {
-  entry: "./lib/webpack.js",
-  mode: "development",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.md$/,
-        use: ["html-loader","@zzf-webpack/loader"],
-      },
+    entry: "./lib/webpack.js",
+    mode: "development",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.md$/,
+                use: ["html-loader", "@zzf-webpack/loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({title: "Hot Module Replacement"}),
+        new DingTalkMsgPlugin({
+            webhook: 'https://oapi.dingtalk.com/robot/send?access_token=xxxx',
+            secret: 'xxxxx'
+        })
     ],
-  },
-  plugins: [new HtmlWebpackPlugin({ title: "Hot Module Replacement" })],
-  devServer: {
-    inline: true,
-    // hot:false
-    hotOnly: true,
-    open: true,
-  },
+    devServer: {
+        inline: true,
+        // hot:false
+        hotOnly: true,
+        open: true,
+    },
 };
